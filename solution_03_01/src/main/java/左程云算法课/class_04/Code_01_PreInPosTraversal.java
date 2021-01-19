484d0290878749468f02577f962ad4cb57d01eec
can19960229 <1361050253@qq.com>
2021-01-19 20:04:53 +0800

    二叉树的前中后遍历（递归和非递归方式）
    Signed-off-by: can19960229 <1361050253@qq.com>

---
+++ solution_03_01/src/main/java/左程云算法课/class_04/Code_01_PreInPosTraversal.java
@@ -0,0 +1,194 @@
+package 左程云算法课.class_04;
+
+import java.util.Stack;
+
+/**
+ * Copyright (C), 2019-2020
+ * author  candy_chen
+ * date   2021/1/19 19:31
+ * version 1.0
+ * Description: 实现二叉树的先序、中序、后序遍历，包括递归方式和非递归方式
+ */
+
+
+
+public class Code_01_PreInPosTraversal {
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    /**
+     * 先序遍历的递归方式
+     * @param head
+     */
+    public static void preOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.value + " ");
+        preOrderRecur(head.left);
+        preOrderRecur(head.right);
+    }
+
+    /**
+     * 中序遍历的递归方式
+     * @param head
+     */
+    public static void inOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        inOrderRecur(head.left);
+        System.out.print(head.value + " ");
+        inOrderRecur(head.right);
+    }
+
+    /**
+     * 后序遍历的递归方式
+     * @param head
+     */
+    public static void posOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        posOrderRecur(head.left);
+        posOrderRecur(head.right);
+        System.out.print(head.value + " ");
+    }
+
+    /**
+     * 前序遍历的非递归方式
+     * @param head
+     */
+    public static void preOrderUnRecur(Node head) {
+        System.out.print("pre-order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<Node>();
+            stack.add(head);
+            while (!stack.isEmpty()) {
+                head = stack.pop();
+                System.out.print(head.value + " ");
+                if (head.right != null) {
+                    stack.push(head.right);
+                }
+                if (head.left != null) {
+                    stack.push(head.left);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * 中序遍历的非递归方式
+     * @param head
+     */
+    public static void inOrderUnRecur(Node head) {
+        System.out.print("in-order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<Node>();
+            while (!stack.isEmpty() || head != null) {
+                //当前节点会把左边的都压进去，一压压一列
+                //当前节点不为空，当前节点压入栈，当前节点向左
+                if (head != null) {
+                    stack.push(head);
+                    head = head.left;
+                } else {
+                    //当前节点为空，从栈哪一个，打印，当前节点向右
+                    head = stack.pop();
+                    System.out.print(head.value + " ");
+                    head = head.right;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    /**
+     * 后序遍历的非递归方式
+     * @param head
+     */
+    public static void posOrderUnRecur1(Node head) {
+        System.out.print("pos-order: ");
+        if (head != null) {
+            Stack<Node> s1 = new Stack<Node>();
+            Stack<Node> s2 = new Stack<Node>();
+            s1.push(head);
+            while (!s1.isEmpty()) {
+                head = s1.pop();
+                s2.push(head);
+                if (head.left != null) {
+                    s1.push(head.left);
+                }
+                if (head.right != null) {
+                    s1.push(head.right);
+                }
+            }
+            while (!s2.isEmpty()) {
+                System.out.print(s2.pop().value + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void posOrderUnRecur2(Node h) {
+        System.out.print("pos-order: ");
+        if (h != null) {
+            Stack<Node> stack = new Stack<Node>();
+            stack.push(h);
+            Node c = null;
+            while (!stack.isEmpty()) {
+                c = stack.peek();
+                if (c.left != null && h != c.left && h != c.right) {
+                    stack.push(c.left);
+                } else if (c.right != null && h != c.right) {
+                    stack.push(c.right);
+                } else {
+                    System.out.print(stack.pop().value + " ");
+                    h = c;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(5);
+        head.left = new Node(3);
+        head.right = new Node(8);
+        head.left.left = new Node(2);
+        head.left.right = new Node(4);
+        head.left.left.left = new Node(1);
+        head.right.left = new Node(7);
+        head.right.left.left = new Node(6);
+        head.right.right = new Node(10);
+        head.right.right.left = new Node(9);
+        head.right.right.right = new Node(11);
+
+        // recursive
+        System.out.println("==============recursive==============");
+        System.out.print("pre-order: ");
+        preOrderRecur(head);
+        System.out.println();
+        System.out.print("in-order: ");
+        inOrderRecur(head);
+        System.out.println();
+        System.out.print("pos-order: ");
+        posOrderRecur(head);
+        System.out.println();
+
+        // unrecursive
+        System.out.println("============unrecursive=============");
+        preOrderUnRecur(head);
+        inOrderUnRecur(head);
+        posOrderUnRecur1(head);
+        posOrderUnRecur2(head);
+
+    }
+}
